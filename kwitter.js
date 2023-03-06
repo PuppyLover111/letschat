00458224b070270425357b596f76738d1c41bad7
Lilly <108691101+PuppyLover111@users.noreply.github.com>
2023-03-06 19:06:10 +0000

    Add files via upload

--- kwitter.js
+++ kwitter.js
@@ -1,3 +1,17 @@
+
+const firebaseConfig = {
+    apiKey: "AIzaSyAFZy3suKfimcjmTIMgz91U5CyGeQl9yxA",
+    authDomain: "letschat-9b524.firebaseapp.com",
+    databaseURL: "https://letschat-9b524-default-rtdb.firebaseio.com",
+    projectId: "letschat-9b524",
+    storageBucket: "letschat-9b524.appspot.com",
+    messagingSenderId: "381440108808",
+    appId: "1:381440108808:web:accdb1109a7d23f009e4bd"
+  };
+  
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
+
 function fly() 
 {
     var user_name = document.getElementById("user_name").value;
